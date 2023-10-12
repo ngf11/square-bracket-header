941d1ffeea5b8bfef1f60ce7d0bf57b0e5f0b488
Nicolas Fuentes <nfuentes11@gmail.com>
2023-10-12 14:47:29 -0400

    First item in the array is added to the dom and removed of the dom

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 const h1 = document.querySelector(".cool-header");
-const h2 = document.querySelector(".cool-header2");
+
+const h1Phreases = ["name is nico.", "Full Stack Developer"];
+
+let i = 0;
+
+let j = 0;
+
+let curremtPhrase = [];
+
+let isDeleting = false;
+
+function loop() {
+  h1.innerHTML = curremtPhrase.join("");
+  if (i < h1Phreases.length) {
+    if (!isDeleting && j <= h1Phreases[i].length) {
+      curremtPhrase.push(h1Phreases[i][j]);
+      j++;
+    }
+
+    if (isDeleting && j <= h1Phreases[i].length) {
+      curremtPhrase.pop(h1Phreases[i][j]);
+      j--;
+    }
+
+    if (j == h1Phreases[i].length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(loop, 500);
+}
+
+loop();
